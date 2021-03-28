539d9c253232c7e95e266751e51dcea3461ae1d9
Brian <brianflaherty64@gmail.com>
2021-03-28 13:42:36 -0400

    fix: resolved some bugs and got package working

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,12 @@ const audioFileToLinear16 = require('../src/lib/audioFileToLinear16');
 
 const program = new Command();
 
+const run = pipeline => (...params) => pipeline(...params).subscribe(
+  console.log,
+  console.error,
+  () => console.log('Complete')
+);
+
 // node ./bin/code-generators/generateScaffold --help
 program
   .command('from-file <inputFilePath> <outputFilePath>')
@@ -12,7 +18,7 @@ program
   // .option('--dry-run', 'write output to console instead of files')
   // .option('-w --without', 'model, create, remove, delete, update')
   // .option('-f --force', 'overwrite existing files')
-  .action((inputFilePath, outputFilePath) => audioFileToLinear16(
+  .action((inputFilePath, outputFilePath) => run(audioFileToLinear16)(
     inputFilePath,
     outputFilePath
   ));
